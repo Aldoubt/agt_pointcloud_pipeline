228fa808691e62820196b887367e1d661cbf680e
Xuan Yang <xuanyang.robotics@gmail.com>
2026-09-11 13:16:38 +0800

    test: register pytest for pointcloud tools

--- src/agt_pointcloud_tools/setup.py
+++ src/agt_pointcloud_tools/setup.py
@@ -11,6 +11,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools', 'PyYAML'],
+    tests_require=['pytest'],
     zip_safe=True,
     maintainer='Xuan Yang',
     maintainer_email='xuanyang.robotics@gmail.com',
